3b16acf1b7ca215d3bc1e5c7e4b8a9a730217055
Stephan <stephan.pirner93@gmail.com>
2019-04-26 01:42:39 +0200

    sample files

---
+++ book_erlen/chapter_02/bsp10001.cpp
@@ -0,0 +1,9 @@
+/* bsp10001.cpp */
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Sample Text";
+	cout << "\nTest me\n";
+	return 0;
+}
---
+++ book_erlen/chapter_02/bsp10002.cpp
@@ -0,0 +1,12 @@
+/* bsp10002.cpp */
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int w;
+	cout << "Insert integer value for w\n";
+	cin >> w;
+	cout << "\nw 2 times is: " << w*2 << "\n";
+	return 0;
+}

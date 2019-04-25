c5777eccefe28ba818890132900f70c0d801cd15
Stephan <stephan.pirner93@gmail.com>
2019-04-26 01:57:50 +0200

    add first exercise from book

---
+++ book_erlen/chapter_02/aufg03.cpp
@@ -0,0 +1,14 @@
+/* Aufg04.cpp*/
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int w;
+	cout << "put number in: ";
+	cin >> w;
+	cout << "double: " << w*2 << "\n";
+	cout << "triple: " << w*3 << "\n";
+	return 0;
+}
